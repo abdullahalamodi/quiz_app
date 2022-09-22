1f266def24daed8077399c62a3665970f4b881d1
alamodi <alamodi326@gmail.com>
2022-09-22 17:44:13 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz_app.dart';
+
+void main() {
+  runApp(const QuizApp());
+}
---
+++ lib/quiz_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
