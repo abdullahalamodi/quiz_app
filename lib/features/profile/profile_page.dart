b5c5ffabe45bfb3514358b5714d2a35c73a17ea4
alamodi <alamodi326@gmail.com>
2022-09-28 17:19:35 +0300

    show error widget in profile page

--- lib/features/profile/profile_page.dart
+++ lib/features/profile/profile_page.dart
@@ -20,72 +20,82 @@ class ProfilePage extends ConsumerWidget {
     return Container(
       padding: kPagePadding,
       child: asyncData.when(
-        data: (data) => Column(
-          children: [
-            const SizedBox(height: 20),
-            ProfileWidget(user: data),
-            const SizedBox(height: 6),
-            const Divider(),
-            const SizedBox(height: 6),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        data: (data) => data != null
+            ? Column(
                 children: [
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 14, vertical: 6),
-                    decoration: BoxDecoration(
-                      color: primaryColor.withOpacity(.15),
-                      borderRadius: BorderRadius.circular(kRadius),
-                    ),
-                    child: Text(
-                      'My Scores',
-                      style: context.tthm.titleMedium!.copyWith(
-                        color: primaryColor,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Consumer(builder: (context, ref, child) {
-                    final asyncScoresList = ref.watch(myScoresProvider);
-                    return Expanded(
-                      child: asyncScoresList.when(
-                        data: (data) => data.isNotEmpty
-                            ? ListView.builder(
-                                itemCount: data.length,
-                                itemBuilder: (context, index) =>
-                                    MyScoreListItem(
-                                  item: data[index],
-                                ),
-                              )
-                            : Center(
-                                child: Column(
-                                  children: [
-                                    SvgPicture.asset(
-                                      FixedAssets.questionIllus,
-                                      height: 150,
-                                    ),
-                                    const SizedBox(height: 30),
-                                    const Text('Your score list is empty.'),
-                                  ],
-                                ),
-                              ),
-                        loading: () => const Center(
-                          child: Text('loading data ..'),
-                        ),
-                        error: (error, stackTrace) => Center(
-                          child: CustomErrorWidget(
-                            onPressed: () => ref.refresh(myScoresProvider),
+                  const SizedBox(height: 20),
+                  ProfileWidget(user: data),
+                  const SizedBox(height: 6),
+                  const Divider(),
+                  const SizedBox(height: 6),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 14, vertical: 6),
+                          decoration: BoxDecoration(
+                            color: primaryColor.withOpacity(.15),
+                            borderRadius: BorderRadius.circular(kRadius),
+                          ),
+                          child: Text(
+                            'My Scores',
+                            style: context.tthm.titleMedium!.copyWith(
+                              color: primaryColor,
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  }),
+                        const SizedBox(height: 12),
+                        Consumer(builder: (context, ref, child) {
+                          final asyncScoresList = ref.watch(myScoresProvider);
+                          return Expanded(
+                            child: asyncScoresList.when(
+                              data: (data) => data.isNotEmpty
+                                  ? ListView.builder(
+                                      itemCount: data.length,
+                                      itemBuilder: (context, index) =>
+                                          MyScoreListItem(
+                                        item: data[index],
+                                      ),
+                                    )
+                                  : Center(
+                                      child: Column(
+                                        children: [
+                                          SvgPicture.asset(
+                                            FixedAssets.questionIllus,
+                                            height: 150,
+                                          ),
+                                          const SizedBox(height: 30),
+                                          const Text(
+                                              'Your score list is empty.'),
+                                        ],
+                                      ),
+                                    ),
+                              loading: () => const Center(
+                                child: Text('loading data ..'),
+                              ),
+                              error: (error, stackTrace) => Center(
+                                child: CustomErrorWidget(
+                                  onPressed: () =>
+                                      ref.refresh(myScoresProvider),
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  )
                 ],
+              )
+            : Center(
+                child: CustomErrorWidget(
+                  onPressed: () {
+                    ref.refresh(profileProvider);
+                  },
+                ),
               ),
-            )
-          ],
-        ),
         loading: () => const Center(
           child: Text('loading data ..'),
         ),
